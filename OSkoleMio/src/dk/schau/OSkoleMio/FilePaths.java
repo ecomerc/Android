ee6434c8b5acc1cbac6656ec1b9a496d5ef2d8bf
Brian Schau <brian@schau.dk>
2012-08-06 20:40:08 +0200

    Changed update URL

--- OSkoleMio/src/dk/schau/OSkoleMio/FilePaths.java
+++ OSkoleMio/src/dk/schau/OSkoleMio/FilePaths.java
@@ -9,7 +9,7 @@ public class FilePaths
 	private static final String _DOWNLOADFOLDER = "/OSkoleMio/downloads/";
 	private static final String _EXTERNALSCHOOLSFILE = "/OSkoleMio/schools.xml";
 	private static final String _INTERNALSCHOOLSFILE = "schools.xml.png";
-	private static final String _WEBSCHOOLSFILE = "http://www.schau.dk/oskolemio/schools.xml";
+	private static final String _WEBSCHOOLSFILE = "http://www.oskolemio.dk/schools.xml";
 
 	public static String getDownloadFolder()
 	{
